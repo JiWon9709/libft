da22e1ba4cd1de92c7a238e5047d037b8716a702
Jiwon You <jyou@student.42seoul.kr>
2020-11-07 15:59:14 +0900

    strrchr finish

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: jyou <jyou@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/10 15:06:23 by jyou              #+#    #+#             */
-/*   Updated: 2020/11/01 17:30:06 by jyou             ###   ########.fr       */
+/*   Updated: 2020/11/07 15:57:29 by jyou             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,14 @@ char	*ft_strrchr(const char *s, int c)
 	char	*next;
 
 	str = (char *)s;
-	if (c == '\0')
-		return (str);
+	next = NULL;
 	while (*str)
 	{
 		if (*str == c)
 			next = str;
 		str++;
 	}
-	if (*next)
-		return (next);
-	else
-		return (NULL);
+	if (c == *str)
+		return ((char *)str);
+	return (next);
 }
