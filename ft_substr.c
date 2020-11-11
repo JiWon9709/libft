3395b7f6228f93868f33bd4059d87d2bc90be702
jyou <jyou@student.42.kr>
2020-11-11 20:42:42 +0900

    split, substr finish

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: jyou <jyou@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/14 17:03:10 by jyou              #+#    #+#             */
-/*   Updated: 2020/11/11 20:19:26 by jyou             ###   ########.fr       */
+/*   Updated: 2020/11/11 20:41:06 by jyou             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,13 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char			*sub;
 	unsigned int	s_len;
 
-	s_len = ft_strlen(s);
-	sub = (char *)malloc(sizeof(char) * (len + 1));
-	if (!s || !sub)
+	if (!s)
 		return (NULL);
+	s_len = ft_strlen(s);
 	if (s_len < start)
 		len = 0;
+	if (!(sub = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
 	ft_memmove(sub, s + start, len);
 	*(sub + len) = '\0';
 	return (sub);
